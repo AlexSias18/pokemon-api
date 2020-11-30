9340b7dfd0f8b001ebe5056b7b8a934553d3e117
Alex Fernando Sias Shaw <42507930+AlexSias18@users.noreply.github.com>
2020-11-29 21:00:09 -0500

    Update Header.js

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     <div class="Header-main">
       <div class="Header-logo">
         <h1>
-          <a href="/">
+          <a href="#about/">
             Pokemon Characters
           </a>
         </h1>
